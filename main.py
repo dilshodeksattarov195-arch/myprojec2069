6b1d99e9b6afd5e02240b67449cd8d0e91ad065a
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 06:38:30 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class smsPyncWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = smsPyncWorker()
+    print("Worker engine initialized.")
